3c9147dc1df25fec425510fd5b11dc9ea9e168e5
StephenDoan123 <longdh5029@gmail.com>
2026-01-16 21:04:05 +0700

    Update function related to memory constraint in Dispatcher.java

--- Dispatcher/Dispatcher.java
+++ Dispatcher/Dispatcher.java
@@ -3,39 +3,144 @@ import Process.*;
 import java.util.*;
 
 public class Dispatcher {
-    private final List<PCB> jobQueue;
+    private final List<PCB> allJobs;
     private final Scheduler scheduler;
+
+    private final List<PCB> blockedQueue = new ArrayList<>();
+    private final List<PCB> suspendQueue = new ArrayList<>();
+
     private int currentTime = 0;
     private PCB running = null;
     private int rrRemaining = 0;
     private long tickSleepMillis;
 
+    private final int totalMemory;
+    private int usedMemory = 0;
+
     public Dispatcher(List<PCB> jobs, Scheduler scheduler){
-        this.jobQueue = new ArrayList<>(jobs);
-        this.jobQueue.sort(Comparator.comparingInt(PCB::getArrivalTime));
-        this.scheduler = scheduler;
+        this(jobs, scheduler, 0, 999999);
     }
 
-    public Dispatcher(List<PCB> jobs, Scheduler scheduler, long tickSleepMillis){
-        this.jobQueue = new ArrayList<>(jobs);
-        this.jobQueue.sort(Comparator.comparingInt(PCB::getArrivalTime));
+    public Dispatcher(List<PCB> jobs, Scheduler scheduler, long tickSleepMillis, int totalMemory){
+        this.allJobs = new ArrayList<>(jobs);
+        this.allJobs.sort(Comparator.comparingInt(PCB::getArrivalTime));
         this.scheduler = scheduler;
         this.tickSleepMillis = tickSleepMillis;
+        this.totalMemory = totalMemory;
     }
 
     private void admitArrivals(){
-        Iterator<PCB> it = jobQueue.iterator();
+        Iterator<PCB> it = allJobs.iterator();
         while (it.hasNext()){
             PCB p = it.next();
             if (p.getArrivalTime() <= currentTime){
-                p.setState(State.READY);
-                scheduler.addProcess(p);
+                // Put to RAM
+                if (usedMemory + p.getMemoryRequired() <= totalMemory) {
+                    allocateMemory(p);
+                    p.setState(State.READY);
+                    scheduler.addProcess(p);
+                    System.out.printf("Time %d: PID %d -> READY (RAM)\n", currentTime, p.getPID());
+                }
+                else {
+                    // RAM full
+                    p.setState(State.READY_SUSPENDED);
+                    suspendQueue.add(p);
+                    System.out.printf("Time %d: PID %d -> READY/SUSPENDED (RAM)\n", currentTime, p.getPID());
+                }
                 it.remove();
-                System.out.printf("Current time = %d: ADMIT %s\n", currentTime, p);
             }
             else break;
         }
     }
+
+    private void processIO() {
+        // BLOCKED
+        Iterator<PCB> it = blockedQueue.iterator();
+        while (it.hasNext()){
+            PCB p = it.next();
+            p.processCurrentInstruction(); // decrease I/O duration
+
+            // If current I/O done, move to the next instr
+            Instruction next = p.getCurrentInstruction();
+            if (next == null || next.getType() == Instruction.Type.CPU) {
+                p.setState(State.READY);
+                scheduler.addProcess(p);
+                it.remove();
+                System.out.printf("Time %d: PID %d finished I/O -> READY\n", currentTime, p.getPID());
+            }
+        }
+
+        // BLOCKED/SUSPENDED
+        for (PCB p: suspendQueue) {
+            if (p.getState() == State.BLOCKED_SUSPENDED){
+                p.processCurrentInstruction();
+                Instruction next = p.getCurrentInstruction();
+                if (next == null || next.getType() == Instruction.Type.CPU){
+                    p.setState(State.READY_SUSPENDED);
+                    System.out.printf("Time %d: PID %d finished I/O (Disk) -> READY/SUSPENDED\n", currentTime, p.getPID());
+                }
+            }
+        }
+    }
+
+    private void swapper() {
+        // Find READY/SUSPENDED process
+        PCB candidate = null;
+        for (PCB p: suspendQueue) {
+            if (p.getState() == State.READY_SUSPENDED) {
+                candidate = p;
+                break;
+            }
+        }
+
+        if (candidate != null) {
+            // RAM enough
+            if (usedMemory + candidate.getMemoryRequired() <= totalMemory) {
+                swapIn(candidate);
+            }
+            // RAM full
+            else {
+                PCB victim = findVictimToSwapOut(candidate.getMemoryRequired());
+                if (victim != null) {
+                    swapOut(victim);
+                    // try swap in
+                    if (usedMemory + candidate.getMemoryRequired() <= totalMemory) {
+                        swapIn(candidate);
+                    }
+                }
+            }
+        }
+    }
+
+    private void swapIn(PCB p) {
+        suspendQueue.remove(p);
+        allocateMemory(p);
+        p.setState(State.READY);
+        scheduler.addProcess(p);
+        System.out.printf("Time %d: SWAP IN PID %d\n", currentTime, p.getPID());
+    }
+
+    private void swapOut(PCB p) {
+        if (p.getState() == State.BLOCKED) {
+            blockedQueue.remove(p);
+            p.setState(State.BLOCKED_SUSPENDED);
+            freeMemory(p);
+            suspendQueue.add(p);
+            System.out.printf("Time %d: SWAP OUT PID %d (Victim)\n", currentTime, p.getPID());
+        }
+    }
+
+    private PCB findVictimToSwapOut(int neededMem) {
+        // find process with 'neededMem' memory in blocked queue
+        for (PCB p: blockedQueue){
+            if (p.getMemoryRequired() >= neededMem) return p;
+        }
+        if (!blockedQueue.isEmpty()) return blockedQueue.get(0);
+        return null;
+    }
+
+    private void allocateMemory(PCB p){usedMemory += p.getMemoryRequired();}
+    private void freeMemory(PCB p){usedMemory -= p.getMemoryRequired();}
 
     private void sleepIfNeeded(){
         if (tickSleepMillis > 0){
@@ -48,81 +153,92 @@ public class Dispatcher {
     }
 
     public void run(){
-        while(true){
+        while(true) {
             admitArrivals();
+            processIO();
+            swapper();
+
             // if no process is running, pick one
-            if (running == null){
+            if (running == null) {
                 running = scheduler.nextProcess(currentTime);
-                if (running != null){
+                if (running != null) {
                     running.setState(State.RUNNING);
                     running.setStartTime(currentTime);
-                    System.out.printf("Current time = %d: DISPATCH %s\n", currentTime, running);
-                    // if RR, set rrRemaining
-                    if (scheduler instanceof RoundRobinScheduler){
+                    System.out.printf("Time %d: DISPATCH %s\n", currentTime, running);
+                    if (scheduler instanceof RoundRobinScheduler) {
                         rrRemaining = ((RoundRobinScheduler) scheduler).getQuota();
                     }
                 }
             }
 
-            // Execute one tick
             if (running != null) {
-                running.decRemaining();
-                currentTime++;
+                Instruction currentOp = running.getCurrentInstruction();
 
-                sleepIfNeeded();
+                // Running CPU instr
+                if (currentOp != null && currentOp.getType() == Instruction.Type.CPU) {
+                    running.processCurrentInstruction();
+                    currentTime++;
 
-                // allow scheduler to update internal state if needed
-                scheduler.onTick(currentTime);
-                admitArrivals();
+                    scheduler.onTick(currentTime);
 
-                // check termination
-                if (running.getRemainingTime() == 0){
-                    running.setState(State.TERMINATED);
-                    running.setCompletionTime(currentTime);
-                    System.out.printf("Current time = %d: TERMINATED %s (turn around = %d, waiting = %d\n",
-                            currentTime, running, running.getTurnaroundTime(), running.getWaitingTime());
-                    running = null;
-                }
-                else{
-                    // Check quota expiration
-                    if (scheduler instanceof RoundRobinScheduler){
+                    if (running.isFinished()) {
+                        running.setState(State.TERMINATED);
+                        running.setCompletionTime(currentTime);
+                        freeMemory(running);
+
+                        System.out.printf("Time %d: %s TERMINATED\n", currentTime, running);
+                        running = null;
+                    }
+                    // Check I/O instr
+                    else if (running.getCurrentInstruction().getType() == Instruction.Type.IO) {
+                        running.setState(State.BLOCKED);
+                        blockedQueue.add(running);
+
+                        System.out.printf("Time %d: PID %d Request I/O -> BLOCKED\n", currentTime, running.getPID());
+                        running = null;
+                    }
+                    // Check RR Quota
+                    else if (scheduler instanceof RoundRobinScheduler) {
                         rrRemaining--;
-                        if (rrRemaining <= 0){
+                        if (rrRemaining <= 0) {
                             running.setState(State.READY);
                             ((RoundRobinScheduler) scheduler).addProcess(running);
-                            System.out.printf("Current time = %d: RR quota expired -> preempt %s\n", currentTime, running);
+
+                            System.out.printf("Time %d: RR Quota expired -> PID %d READY\n", currentTime, running.getPID());
                             running = null;
                         }
                     }
-                    // Check preemption
-                    else if (scheduler.isPreemptive() && scheduler instanceof PriorityScheduler){
+                    // Check priority
+                    else if (scheduler.isPreemptive() && scheduler instanceof PriorityScheduler) {
                         PriorityScheduler ps = (PriorityScheduler) scheduler;
                         PCB best = ps.peek();
                         if (best != null) {
                             int cmp = Integer.compare(best.getPriority(), running.getPriority());
-                            if ((cmp < 0 && ps.isPreemptive()) || (cmp != 0 && ps.isPreemptive())){
-                                //preempt
-                                running.setState(State.READY);
-                                ps.addProcess(running);
-                                System.out.printf("Current time = %d: PREEMPT %s due to %s\n", currentTime, running, best);
-                                running = null;
-                            }
                         }
                     }
                 }
+
+                // Running I/O instr
+                else if (currentOp != null && currentOp.getType() == Instruction.Type.IO) {
+                    running.setState(State.BLOCKED);
+                    blockedQueue.add(running);
+                    running = null;
+                }
             }
-            else{
-                // idle
-                if (jobQueue.isEmpty() && (scheduler.nextProcess(currentTime) == null)){
-                    // finished (jobQueue and ready are empty)
+            else {
+                // CPU idle
+                if (allJobs.isEmpty() && running == null && blockedQueue.isEmpty()&& suspendQueue.isEmpty() && schedulerHasNoProcess()) {
                     break;
                 }
-                System.out.printf("Current time = %d: CPU IDLE\n", currentTime);
+                System.out.printf("Time %d: CPU idle\n", currentTime);
                 currentTime++;
-
-                sleepIfNeeded();
             }
+            sleepIfNeeded();
         }
         System.out.printf("Simulation finished at t = %d\n", currentTime);
+    }
+
+    private boolean schedulerHasNoProcess() {
+        return scheduler.nextProcess(currentTime) == null;
     }
 }

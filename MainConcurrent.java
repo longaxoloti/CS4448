97381f0467d7f167fd47aebf48d8b125ba3405a4
StephenDoan123 <longdh5029@gmail.com>
2026-01-16 21:04:38 +0700

    Update main file

--- MainConcurrent.java
+++ MainConcurrent.java
@@ -10,21 +10,38 @@ public class MainConcurrent {
         PrintStream orig = System.out;
         System.setOut(new PrefixedPrintStream(orig));
 
-        List<PCB> jobA = Arrays.asList(
-                new PCB(1, 2, 5, 0),
-                new PCB(2, 1, 3, 1),
-                new PCB(3, 3, 2, 2)
+        // Job A1: CPU(5), RAM(100)
+        List<Instruction> insA1 = Arrays.asList(new Instruction(Instruction.Type.CPU, 5));
+
+        // Job A2: CPU(2) -> IO(3) -> CPU(1), RAM(200)
+        List<Instruction> insA2 = Arrays.asList(
+                new Instruction(Instruction.Type.CPU, 2),
+                new Instruction(Instruction.Type.IO, 3),
+                new Instruction(Instruction.Type.CPU, 1)
         );
 
-        List<PCB> jobB = Arrays.asList(
-                new PCB(11, 1, 4, 0),
-                new PCB(12, 2, 6, 2),
-                new PCB(13, 1, 2, 3)
+        // Job A3: CPU(3), RAM(150)
+        List<Instruction> insA3 = Arrays.asList(new Instruction(Instruction.Type.CPU, 3));
+
+        List<PCB> jobA = Arrays.asList(
+                new PCB(1, 2, 100, 0, insA1),
+                new PCB(2, 1, 200, 1, insA2),
+                new PCB(3, 3, 150, 2, insA3)
         );
+
+        List<PCB> jobB = new ArrayList<>();
+        // Job B1: CPU(4), RAM 300
+        jobB.add(new PCB(11, 1, 300, 0, Arrays.asList(new Instruction(Instruction.Type.CPU, 4))));
+        // Job B2: CPU(2) -> IO(5) -> CPU(2), RAM 400
+        jobB.add(new PCB(12, 2, 400, 2, Arrays.asList(
+                new Instruction(Instruction.Type.CPU, 2),
+                new Instruction(Instruction.Type.IO, 5),
+                new Instruction(Instruction.Type.CPU, 2)
+        )));
 
         // Create modules
-        ThreadModule modRR = new ThreadModule("RR", jobA, 2, 20L, false, true);
-        ThreadModule modPR = new ThreadModule("Prio", jobB, 0, 20L, true, true);
+        ThreadModule modRR = new ThreadModule("RR", jobA, 2, 20L, false, true, 500);
+        ThreadModule modPR = new ThreadModule("Prio", jobB, 0, 20L, true, true, 500);
 
         Thread tRR = new Thread(modRR, "[RR]");
         Thread tPR = new Thread(modPR, "[PRIO]");
--- Problem1.java
+++ Problem1.java
@@ -4,26 +4,36 @@ import Process.*;
 import java.util.*;
 public class Problem1 {
     public static void main(String[] args){
-        List<PCB> jobs = Arrays.asList(
-                new PCB(1, 2, 5, 0),
-                new PCB(2, 1, 3, 1),
-                new PCB(3, 3, 2, 2),
-                new PCB(4, 2, 4, 3)
+
+        System.out.println("=== Round Robin (Time Quantum = 2, RAM = 500MB) ===");
+
+        List<Instruction> ins1 = Arrays.asList(new Instruction(Instruction.Type.CPU, 4));
+        List<Instruction> ins2 = Arrays.asList(
+                new Instruction(Instruction.Type.CPU, 2),
+                new Instruction(Instruction.Type.IO, 5),
+                new Instruction(Instruction.Type.CPU, 2)
         );
-        System.out.println("=== Round Robin (q = 2) ===");
+        List<Instruction> ins3 = Arrays.asList(new Instruction(Instruction.Type.CPU, 5));
+
+        List<PCB> jobs = Arrays.asList(
+                new PCB(1, 2, 100, 0, ins1),
+                new PCB(2, 1, 250, 1, ins2),
+                new PCB(3, 3, 200, 2, ins3)
+        );
         Scheduler rr = new RoundRobinScheduler(2);
-        Dispatcher d1 = new Dispatcher(jobs, rr);
+        Dispatcher d1 = new Dispatcher(jobs, rr, 0, 500);
         d1.run();
 
-        System.out.println("\n=== Priority Preemptive (smaller = better) ===");
+        System.out.println("\n=== Priority Preemptive (smaller = better, RAM = 1000MB) ===");
+
+        List<Instruction> ins4 = Arrays.asList(new Instruction(Instruction.Type.CPU, 10));
+        List<Instruction> ins5 = Arrays.asList(new Instruction(Instruction.Type.CPU, 3));
         List<PCB> jobs2 = Arrays.asList(
-                new PCB(1, 2, 5, 0),
-                new PCB(2, 1, 3, 1),
-                new PCB(3, 3, 2, 2),
-                new PCB(4, 2, 4, 3)
+                new PCB(4, 2, 100, 0, ins4),
+                new PCB(5, 1, 100, 2, ins5)
         );
         Scheduler pr = new PriorityScheduler(true, true);
-        Dispatcher d2 = new Dispatcher(jobs2, pr);
+        Dispatcher d2 = new Dispatcher(jobs2, pr, 0, 1000);
         d2.run();
     }
 }
